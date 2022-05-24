df2eea11afc823c27b1c4bdfa2b7ff2888264532
biankiy <biankiygimenez@gmail.com>
2022-05-24 12:19:14 -0300

    c#.net

---
+++ EjercicioClase06/EjercicioClase06/Program.cs
@@ -0,0 +1,67 @@
+﻿/* 
+ * Bianca Gimenez
+ * 
+     1) Generar un número secreto
+    aleatorio con la siguiente instruccion:
+
+    // Genera un numero entero aleatorio de 1 a 20
+    int numeroSecreto = new
+    Random(DateTime.Now.Millisecond).Next(1, 21);
+
+    2) Pedir al usuario que ingrese un número y que
+    intente adivinar el número que eligió la computadora.
+
+    3) Si el numero ingresado por el usuario es mayor
+    al número secreto, avisarle que es muy grande y que intente de nuevo y que
+    vuelva al paso 2.
+
+    4) Si el numero ingresado es menor al número
+    secreto, avisarle que es muy chico, y que intente de nuevo volviendo al paso 2.
+
+    5) Si el número ingresado coincide con el número
+    secreto, sacar el siguiente mensaje:
+
+    "Felicitaciones, has adivinado el número
+    secreto que era:  [numeroSecreto]"
+    "Lo has logrado en [n] intentos!!"
+
+    Y finalizar el programa.
+ */
+
+int numero;
+int intentos = 0;
+//1) Genera un numero entero aleatorio de 1 a 20
+int numeroSecreto = new
+    Random(DateTime.Now.Millisecond).Next(1, 20);
+
+
+do {
+    Console.WriteLine();
+    Console.WriteLine("Intente adivinar el numero que eligio la Computadora");
+    Console.WriteLine("Entre 1-20");
+    Console.WriteLine();
+
+    Console.Write("Ingrese un numero: ");
+#pragma warning disable CS8604 // Posible argumento de referencia nulo
+    numero = int.Parse(s: Console.ReadLine());
+#pragma warning restore CS8604 // Posible argumento de referencia nulo
+
+    if (numero > numeroSecreto)
+    {
+        Console.WriteLine();
+        Console.WriteLine("-El numero secreto es menor al numero ingresado-");
+    }
+    else {
+        Console.WriteLine();
+        Console.WriteLine("-El numero secreto es mayor al numero ingresado-");
+    }
+
+    intentos++;
+} while (numeroSecreto != numero);
+    
+    Console.WriteLine();
+    Console.WriteLine("Felicitaciones, has adivinado el número");
+    Console.WriteLine("secreto que era: "+ numeroSecreto);    
+    Console.WriteLine("Lo has logrado en "+ intentos + " intentos!!");
+
+
---
+++ EjercicioClase07/Ejercicio07/Program.cs
@@ -0,0 +1,54 @@
+﻿/* Bianca Gimenez
+ 
+ Escribe un programa que:
+
+    -1) Pida 10 números al usuario.
+    -2) Obtener la suma de todos los números ingresados.
+    -3) Obtener cual es el mayor de todos los números.
+    -4) Obtener cual es el menor de todos los números.
+    -5) Obtener el promedio de todos los números.
+    -6) Mostrar todos los números ingresados por pantalla.
+    -7) Mostrar los valores calculados en los puntos 2, 3, 4 y 5.
+ */
+
+int[] numeros = new int[10];
+float contador = 0;
+int acumulador = 0;
+float promedio = 0;
+int maximo = 0;
+int minimo = 0;
+
+
+    for (int i = 0; i < numeros.Length; i++) { 
+          Console.Write($"Ingrese un numero para la posicion {i+1}: ");
+
+    numeros[i] = int.Parse(Console.ReadLine());
+    acumulador += numeros[i];
+    contador++;
+
+
+    if (i == 0 || numeros[i] > maximo)
+    {
+        maximo = numeros[i];
+        minimo = numeros[i];
+    }
+    else if (numeros[i] < minimo){
+        minimo = numeros[i];
+    }
+
+    }
+promedio = acumulador / contador;
+
+    Console.WriteLine();
+    Console.WriteLine($"  La suma de los valores ingresados es: {acumulador}");
+    Console.WriteLine();
+    Console.WriteLine($"  El promedio es los valores ingresados es: {promedio}");
+    Console.WriteLine();
+    for (int i = 0; i < numeros.Length; i++) {
+        Console.Write($"{numeros[i]} - ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine($"  El mayor numero ingresado es : {maximo}");
+    Console.WriteLine();
+    Console.WriteLine($"  El menor numero ingresados es: {minimo}");
